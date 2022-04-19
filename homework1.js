677ea9be050219fa9dbcf5f74cb642043ad4f473
Bohdan Bryk <bohdan.bryk@ucu.edu.ua>
2022-04-19 23:18:29 +0300

    add changes

--- homework1.js
+++ homework1.js
@@ -6,5 +6,15 @@ function reverseArray1(str){
     return reversedStr;
 }
 
+function reverseLoop1(str){
+    let newStr = "";
+    let wordLen = str.length;
+    for(let i = wordLen-1; i>=0;i--){
+        newStr += str[i]
+    }
+    return newStr;
+}
 
 console.log(reverseArray1("word"));
+console.log(reverseLoop1("hello"));
+
